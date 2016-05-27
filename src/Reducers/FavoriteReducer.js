797d03afa15a04819ea612da34c94a0cefd29501
lalli@cn.ibm.com <lalli@cn.ibm.com>
2016-05-27 16:50:50 +0800

    avoid redundant favorite list, and fail to use Set, no refresh in JSX

--- src/Reducers/FavoriteReducer.js
+++ src/Reducers/FavoriteReducer.js
@@ -4,7 +4,16 @@ import {ADD_NEW_FAVORITE, REMOVE_FAVORITE} from '../Action/actions';
 export default function favoriteReducer(state=[], action) {
 	switch(action.type){
 		case ADD_NEW_FAVORITE:
-			return [...state, action.payload];
+			/*return [...state.filter((item)=>{
+						return item.id !== action.payload.id;
+					}), action.payload];*/
+			console.log(state.some((item)=>{
+				return item.id === action.payload.id
+			}));
+			return (state.some((item)=>{
+				return item.id === action.payload.id
+			}))?state:[...state, action.payload];		
+					
 		case REMOVE_FAVORITE:
 			return state.filter((item)=>{
 				return item.id !== action.payload.id;
